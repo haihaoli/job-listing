f266f3deaa435e4a65b1a8c83707a6a093ab331d
lihaihao <mashy1024@gmail.com>
2017-01-15 17:46:03 +0800

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,16 @@ Rails.application.routes.draw do
   root 'jobs#index'
   namespace :admin do
     resources :jobs do
+      resources :resumes
       member do
         post :hide
         post :publish
       end
-    resources :resumes
     end
   end
+
+  resources :jobs do
+    resources :resumes
+  end
+
 end
